ee3884dfb39ef36ebfef786cff6b640667c2ebee
hoclentop <94523466+hoclentop@users.noreply.github.com>
2023-09-04 00:45:32 +0700

    Create b221e4.py

---
+++ b221e4.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open("tong.inp","r")
+sys.stdout = open("tong.out","w")
+n = int(input())
+a = [int(x) for x in input().split()]
+smax = -float('inf')
+for i in range(n):
+    s=0
+    for j in range(i,n):
+        s+=a[j]
+        if s > smax:
+            smax = s
+            v = i+1
+            d = j-i+1
+print(smax)
+print(v)
+print(d)
