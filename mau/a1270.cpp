e768c868d55627dc237dcb5c6fd3a0bb0ca88ca8
hoclentop <94523466+hoclentop@users.noreply.github.com>
2023-09-06 09:20:41 +0700

    Update a1270.cpp

--- mau/a1270.cpp
+++ mau/a1270.cpp
@@ -6,7 +6,7 @@ int main()
     int i;
     cin>>n;
     for (i=1; i<=n; i++) cin>>a[i];
-    cout<<a[1];
+    cout<<a[1]<<" ";
     for (i=2; i<=n; i++) {
         if (a[i] <= a[i-1]) cout << endl;
         cout << a[i] <<" ";
