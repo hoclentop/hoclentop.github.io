c47176d1f7df86b90a8b9cb95ccd96c34d000e73
hoclentop <94523466+hoclentop@users.noreply.github.com>
2024-09-03 00:24:52 +0700

    Update de3396.cpp

--- sach/de3396.cpp
+++ sach/de3396.cpp
@@ -8,7 +8,7 @@ int main() {
     for (i=1; i<=n; i++) cin>>c[i]>>p[i];
     for(i=1; i<=n; i++) {
         for (j=1; j<=m; j++) {
-            F[i][i]=F[i-1][j];
+            F[i][j]=F[i-1][j];
             if (j>=c[i])
                 F[i][j]=max(F[i][j], F[i-1][j-c[i]]+p[i]);
             if (fmax < F[i][j]) {
@@ -20,7 +20,7 @@ int main() {
     int cmax=y;
     cout<<fmax<<endl;
     for (x=n; x>=1; x--)
-        if (y>=c[x] && F[x][y] == F[x-1][y-c[i]]+p[x]){
+        if (y>=c[x] && F[x][y] == F[x-1][y-c[x]]+p[x]){
             cout<<x<<" ";
             y-=c[x];
         }
