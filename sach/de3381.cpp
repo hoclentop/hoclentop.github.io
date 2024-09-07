5e8cf00dbdefb8b56139ef97db8f61378db00a24
hoclentop <94523466+hoclentop@users.noreply.github.com>
2024-09-07 23:11:56 +0700

    Update de3381.cpp

--- sach/de3381.cpp
+++ sach/de3381.cpp
@@ -1,28 +1,38 @@
 //DE3381.CPP - Tran Huu Nam - hsgtin.vn - 2024
 #include <bits/stdc++.h>
 using namespace std;
-int x,y,n,a,r,m, h[100001];
-int main()
-{
-    cin>>n>>a>>r>>m;
-    if (m>a+r) m = a+r;
-    for (int i=1; i<=n; i++) cin>>h[i];
-    sort(h+1, h+n+1);
-    x=1; y=n;
-    while (x<y && h[x+1]==h[x]) x++;
-    while (y>x && h[y-1]==h[y]) y--;
-    long long c=0;
-    while (h[y]-h[x]>=2) {
-        c+=m;
-        h[x]++;
-        x--;
-        while (x<y && h[x+1]==h[1]) x++;
-        h[y]--;
-        y++;
-        while (y>x && h[y-1]==h[n]) y--;
+int n, a, r, m;
+int h[100001];
+long long chiphi(int k) {
+    long long sa=0, sr=0, sm;
+    for (int i=1; i<=n; i++) {
+        if (h[i]>=k) sr+=h[i]-k;
+        else sa+= k-h[i];
     }
-    if (x<y) c+=min(x*a, (n-x)*r);
-    cout<<c;
-    return 0;
+    sm=min(sa, sr);
+    return sm*m+(sa-sm)*a+(sr-sm)*r;
 }
 
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+    cin>>n>>a>>r>>m;
+    if (a+r<m) m=a+r;
+    int d=1e9, c=0, p, q, z;
+    for (int i=1; i<=n; i++) {
+        cin>>h[i];
+        if (d>h[i]) d=h[i];
+        if (c<h[i]) c=h[i];
+    }
+    while (d+3<=c) {
+        z=(c-d)/3;
+        p=d+z;
+        q=c-z;
+        if(chiphi(p)>=chiphi(q)) d=p;
+        else c=q;
+    }
+    while (chiphi(d)>=chiphi(d+1)) d++;
+    cout<<chiphi(d);
+    return 0;
+}
