b2bebc2a8474544af66b9b1c877487c2b8882f2a
hoclentop <94523466+hoclentop@users.noreply.github.com>
2024-09-12 18:04:20 +0700

    Update de3387.py

--- sach/de3387.py
+++ sach/de3387.py
@@ -1,24 +1,23 @@
-//DE3387.PY - Tran Huu Nam - hsgtin.vn - 2024
+#DE3387.PY - Tran Huu Nam - hsgtin.vn - 2024
 import sys
-from collections import defaultdict
-def main():
-    c = defaultdict(int)
-    s = ""
-    p = ""
-    n = 0
-    p = input() + 'a'
-    for char in p:
-        if char.isdigit():
-            n = n * 10 + int(char)
-        elif n > 0:
-            break
-    for line in sys.stdin:
-        s += line
-    for char in s:
-        c[char] += 1
-    for char in s:
-        if c[char] <= n:
-            print(char, end='')
-if __name__ == "__main__":
-    main()
+
+n = 0
+p = input() + 'a'
+for x in p:
+    if x.isdigit():
+        n = n * 10 + int(x)
+    elif n > 0:
+        break
+s = ""
+for line in sys.stdin:
+    s += line.strip()
+c=[0]*256
+for y in s:
+    x=ord(y)
+    c[x] += 1
+for y in s:
+    x=ord(y)
+    if c[x] <= n:
+        print(y, end='')
+        c[x]=n+1
 
