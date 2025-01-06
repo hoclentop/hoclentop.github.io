6cabe04501718dc3991e2cc223f49bc44dab2e65
hoclentop <94523466+hoclentop@users.noreply.github.com>
2025-01-06 14:24:01 +0700

    Update de3339.cpp

--- sach/de3339.cpp
+++ sach/de3339.cpp
@@ -10,7 +10,9 @@ int main() {
     for(int i=0;i<t;i++) cin>>res[i];
     sort(a+1,a+n+1);
     a[0]=0; b[0]=0;
-    for(int i=1;i<=n;i++) b[i]=b[i-1]+a[i]-a[i-1]-1;
+    for(int i=1;i<=n;i++) {
+        b[i]=b[i-1]+a[i]-a[i-1]-(a[i]==a[i-1]?0:1);
+    }
     for(int i=0;i<t;i++){
         if(res[i]>b[n]) cout<<a[n]+res[i]-b[n]<<endl;
         else{
